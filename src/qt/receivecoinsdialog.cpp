1e4bee6cb37f5cf5dd7bc7c18fd00fd210017e2c
Unknown <jamy0184@gmail.com>
2019-01-16 14:07:33 +0100

    Fixed issue.
    Fixed an issue regarding receiving coins

--- src/qt/receivecoinsdialog.cpp
+++ src/qt/receivecoinsdialog.cpp
@@ -91,7 +91,7 @@ void ReceiveCoinsDialog::setModel(WalletModel* model)
         address = settings.value("current_receive_address").toString();
         if (address.isEmpty())
             address = getAddress();
-        ui->reqAddress->setText(address);
+        ui->reqAddress->setText("");
 
         connect(model, SIGNAL(notifyReceiveAddressChanged()), this, SLOT(receiveAddressUsed()));
     }
@@ -300,4 +300,3 @@ void ReceiveCoinsDialog::receiveAddressUsed()
         clear();
     }
 }
-
